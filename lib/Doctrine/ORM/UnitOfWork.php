4cc163f9eab6fdc60087531081624c6df15f9245
Derek Lambert <dlambert@dereklambert.com>
2012-07-26 15:52:02 -0500

    If field is a mapped association change target entity to value in descriminator column.

--- lib/Doctrine/ORM/UnitOfWork.php
+++ lib/Doctrine/ORM/UnitOfWork.php
@@ -2407,6 +2407,10 @@ class UnitOfWork implements PropertyChangedListener
                 continue;
             }
 
+            if (isset ($class->mappedAssociations[$field])) {
+                $assoc['targetEntity'] = $data[$class->mappedAssociations[$field]['fieldMapping']['columnName']];
+            }
+
             $targetClass = $this->em->getClassMetadata($assoc['targetEntity']);
 
             switch (true) {
